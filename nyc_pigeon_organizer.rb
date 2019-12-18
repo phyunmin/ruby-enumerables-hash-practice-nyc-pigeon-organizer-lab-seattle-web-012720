14f8b6496f98a89c6975eba9cfa2c962fbf72a89
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 21:55:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    hash = {}
+    hash1 = {}
+    hash2 = {}
+    data.each do |color_gen_lives, values|
+        values.each do |value, array|
+            array.each do |name|
+                if hash[name]==nil
+                    hash[name]={}
+                    hash[name][color_gen_lives]=[]
+                else
+                    hash[name][color_gen_lives]=[]
+                end
+            end
+        end
+    end
+
+    hash.each do |name, values|
+        values.each do |hashvalue, array|
+            data.each do |color_gen_lives, values|
+                values.each do |value, array|
+                    array.each do |element|
+                        if(name == element && hashvalue == color_gen_lives)
+                            hash[name][hashvalue] << value.to_s
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return hash
 end
